fa9f3b75c95f23a30021d6f8cc125a04917c549f
刘稳 <lw3039@ly.com>
2018-11-01 18:19:53 +0800

    no message

--- config/index.js
+++ config/index.js
@@ -6,7 +6,7 @@ module.exports = {
         env: require('./prod.env'),
         index: path.resolve(__dirname, '../manage/index.html'),
         assetsRoot: path.resolve(__dirname, '../manage'),
-        assetsSubDirectory: '',
+        assetsSubDirectory: 'static',
         assetsPublicPath: '/',
         productionSourceMap: false,
         // Gzip off by default as many popular static hosts such as
